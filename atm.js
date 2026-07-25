7200b0529a4133ed15e44663b6a0c6aef617c78c
Nico Benz <snicobenz@gmail.com>
2026-07-25 14:30:42 +0800

    renamed variables to match activity requirements

--- atm.js
+++ atm.js
@@ -1,22 +1,25 @@
-let balance = 500;
+// Language: JavaScript
+// Description: This program simulates a basic ATM withdrawal system.
 
+let account_balance = 500;
 
 // dito is kung saan natin papaganahin ang system sa loob ng function
 function process() {
 
     //where are finding the id in the index.html file 
-    let amount = document.getElementById("withdrawalInput").value;
+    let withdrawal_amount = document.getElementById("withdrawalInput").value;
 
     // converting the value sa amount into float para detailed para kahit my decimal ma process
-    amount = parseFloat(amount);
+    withdrawal_amount = parseFloat(withdrawal_amount);
 
-    // 
+    // hanapin ang message box sa index.html para malagyan ng output
     const messageOuput = document.getElementById("messageOutput");
 
     //here is the procees if ang amount na imong eh withdraw is less tha equal sa balance succes but if exceed sa balance unsuccesful
-    if(amount <= balance) {
-        balance  = balance - amount;
-        messageOuput.innerHTML = "withdraw succes!" + "<br>" + "Your balance is " + change;
+    if(withdrawal_amount <= account_balance) {
+        account_balance = account_balance - withdrawal_amount;
+        // nag gamit ako innerHTML than textContent kasi para gagana ang breakline ko
+        messageOuput.innerHTML = "withdraw succes!" + "<br>" + "Your balance is " + account_balance;
        
 
     } else {
